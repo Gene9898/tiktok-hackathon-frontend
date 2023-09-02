2211a6296c6c42cf72594f991a61a4e0bfb40b34
Jonathan <jonlee121198@gmail.com>
2023-09-03 00:26:25 +0800

    Added google authentication using firebase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import React from "react"
+import { initFirebase } from "@/config/firebase"
+import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth"
 
 export default function Home() {
+  const app = initFirebase()
+  console.log(app)
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const signIn = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result.user)
+  }
+
   return (
   <div>
     <h1>Hi</h1>
+    <button onClick={signIn}>Sign in</button>
   </div>
   )
 }
