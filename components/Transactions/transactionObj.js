330139c5561b4d4eff2aed735f2eb9f74e797ffb
Jermy-Lee <116236000+Jermy-Lee@users.noreply.github.com>
2023-09-06 15:20:04 +0800

    report page

--- components/Transactions/transactionObj.js
+++ components/Transactions/transactionObj.js
@@ -1,35 +1,40 @@
-const React = require('react');
+import React, { useState } from 'react';
+import ReportPopup from 'components/Reports/ReportPopup'; 
+
 const { currencyFormatter } = require("lib/utils");
 
-const handleClick = () => {
-  // Redirect to the report page URL
-  window.location.href = '/report';
-};
+function Transaction({ color, title, amount, id }) {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-const buttonElement = React.createElement(
-  'button',
-  {
-    className: 'report',
-    onClick: handleClick, // Attach the click event handler
-  },
-  'Report'
-);
+  const handleReportClick = () => {
+    setIsPopupOpen(true);
+  };
 
-function Transaction({ color, title, amount }) {
   return (
-    React.createElement("div", { className: "flex items-center justify-between px-4 py-4 bg-slate-800 rounded-3xl" },
-      React.createElement("div", { className: "flex items-center gap-2" },
-        React.createElement("div", {
-          className: "w-[8px] h-[8px] sm:w-[25px] sm:h-[25px] rounded-full",
-          style: { backgroundColor: color }
-        }),
-        React.createElement("h4", { className: "capitalize" }, title)
-      ),
-      React.createElement("div", { className: "flex items-center gap-9" },
-        React.createElement("p", null, currencyFormatter(amount)),
-        buttonElement
-      )
-    )
+    <div className="flex items-center justify-between px-4 py-4 bg-slate-800 rounded-3xl">
+      <div className="flex items-center gap-2">
+        <div
+          className="w-[8px] h-[8px] sm:w-[25px] sm:h-[25px] rounded-full"
+          style={{ backgroundColor: color }}
+        />
+        <h4 className="capitalize">{title}</h4>
+      </div>
+      <div className="flex items-center gap-9">
+        <p>{currencyFormatter(amount)}</p>
+        <button className='report' onClick={handleReportClick}>
+          Report
+        </button>
+      </div>
+
+      {isPopupOpen && (
+        <ReportPopup
+          transactionId={id}
+          title={title}
+          amount={amount}
+          onClose={() => setIsPopupOpen(false)}
+        />
+      )}
+    </div>
   );
 }
 
